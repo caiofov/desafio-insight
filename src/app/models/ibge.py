269d35b77c6bb14546f77fa8e8d46b247d61bacc
caiofov <cfoviana@gmail.com>
2024-06-08 15:58:02 -0300

    fix: models alias

--- src/app/models/ibge.py
+++ src/app/models/ibge.py
@@ -1,5 +1,5 @@
 from app.utils.enums import SiglaRegiao
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Regiao(BaseModel):
@@ -24,7 +24,9 @@ class RegiaoIntermediaria(BaseModel):
 class RegiaoImediata(BaseModel):
     id: int
     nome: str
-    regiao_intermediaria: RegiaoIntermediaria | None
+    regiao_intermediaria: RegiaoIntermediaria | None = Field(
+        alias="regiao-intermediaria"
+    )
 
 
 class Mesorregiao(BaseModel):
@@ -35,22 +37,25 @@ class Mesorregiao(BaseModel):
 class Microrregiao(BaseModel):
     id: int
     nome: str
-    mesorregiao: Mesorregiao | None
+    mesorregiao: Mesorregiao | None = None
 
 
 class MicrorregiaoWithImediata(Microrregiao):
-    regiao_imediata: RegiaoImediata | None
+    regiao_imediata: RegiaoImediata | None = Field(alias="regiao-imediata")
 
 
 class Municipio(BaseModel):
     id: int
     nome: str
-    microrregiao: Microrregiao | MicrorregiaoWithImediata | None
+    microrregiao: Microrregiao | MicrorregiaoWithImediata | None = None
 
 
 class MunicipioWithImediata(Municipio):
-    microrregiao: Microrregiao | None
-    regiao_imediata: RegiaoImediata | None
+    microrregiao: Microrregiao | None = None
+    regiao_imediata: RegiaoImediata = Field(alias="regiao-imediata")
+
+
+MunicipioType = Municipio | MunicipioWithImediata
 
 
 class Distrito(BaseModel):

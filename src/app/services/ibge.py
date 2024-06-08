b5b55f54ec8495ecfe0e18fe368164f46e12e481
caiofov <cfoviana@gmail.com>
2024-06-08 00:11:27 -0300

    refactor: request with filter and pagination

--- src/app/services/ibge.py
+++ src/app/services/ibge.py
@@ -11,38 +11,44 @@ class IBGEService:
         self.base_url = "https://servicodados.ibge.gov.br/api/v1"
         self.localidades_url = f"{self.base_url}/localidades"
 
-    def make_get_request_with_filter(
-        self, url: str, filter: Callable[[dict[str, Any]], bool]
-    ) -> list[dict[str, Any]]:
+    def _request_with_filter_and_pagination(
+        self,
+        url: str,
+        page: int,
+        per_page: int,
+        filter: Callable[[dict[str, Any]], bool],
+        model_constructor: Callable[[dict[str, Any]], T],
+    ) -> list[T]:
         r = requests.get(url)
         r.raise_for_status()
 
-        return [r for r in r.json() if filter(r)]
+        skip = (page - 1) * per_page
+        skipped = 0
+        data = []
 
-    def pagination_with_models(
-        self,
-        data: list[dict[str, Any]],
-        skip: int,
-        per_page: int,
-        model_constructor: Callable[[dict[str, Any]], T],
-    ) -> list[T]:
-        models = []
-        for d in data[skip:]:
-            if len(models) > per_page:
+        for d in r.json():
+            if len(data) >= per_page:
                 break
-            models.append(model_constructor(**d))
-        return models
+
+            if not (accept := filter(d)):
+                continue
+            if accept and skipped < skip:
+                skipped += 1
+                continue
+
+            data.append(model_constructor(**d))
+
+        return data
 
     def list_distritos(
         self, page: int, per_page: int, search: str | None = None
     ) -> list[Distrito]:
-        skip = (page - 1) * per_page
         search = search.lower() if search else None  # not case sensitive
 
-        raw = self.make_get_request_with_filter(
+        return self._request_with_filter_and_pagination(
             f"{self.localidades_url}/distritos",
+            page,
+            per_page,
             lambda d: not search or d["nome"].lower().startswith(search),
+            Distrito,
         )
-
-        # transform to base model and pagination
-        return self.pagination_with_models(raw, skip, per_page, Distrito)

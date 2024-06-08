d4d3e31c7e5a690cdf91a226cab54e0d1be0f932
caiofov <cfoviana@gmail.com>
2024-06-08 15:19:51 -0300

    feat: add ibge client to service

--- src/app/factories/ibge.py
+++ src/app/factories/ibge.py
@@ -1,6 +1,14 @@
 from typing import Iterator
 from app.services.ibge import IBGEService
+from app.utils.ibge_client import IBGELocalidadesClient
+from fastapi import Depends
 
 
-def get_ibge_service() -> Iterator[IBGEService]:
-    yield IBGEService()
+def get_ibge_client() -> Iterator[IBGELocalidadesClient]:
+    yield IBGELocalidadesClient()
+
+
+def get_ibge_service(
+    _ibge_client: IBGELocalidadesClient = Depends(get_ibge_client),
+) -> Iterator[IBGEService]:
+    yield IBGEService(_ibge_client)
--- src/app/services/ibge.py
+++ src/app/services/ibge.py
@@ -1,32 +1,31 @@
 from app.models.ibge import Distrito, Municipio
 from typing import Any, Callable, TypeVar
+from app.utils.ibge_client import IBGELocalidadesClient
+from app.utils.types import RawJSONType
 from pydantic import BaseModel
-import requests
 
 T = TypeVar("T", bound=BaseModel)
 
 
 class IBGEService:
-    def __init__(self) -> None:
+    def __init__(self, _ibge_client: IBGELocalidadesClient) -> None:
         self.base_url = "https://servicodados.ibge.gov.br/api/v1"
         self.localidades_url = f"{self.base_url}/localidades"
+        self.ibge_client = _ibge_client
 
-    def _request_with_filter_and_pagination(
+    def _filter_and_pagination(
         self,
-        url: str,
+        raw_data: list[RawJSONType],
         page: int,
         per_page: int,
         filter: Callable[[dict[str, Any]], bool],
         model_constructor: Callable[[dict[str, Any]], T],
     ) -> list[T]:
-        r = requests.get(url)
-        r.raise_for_status()
-
         skip = (page - 1) * per_page
         skipped = 0
         data = []
 
-        for d in r.json():
+        for d in raw_data:
             if len(data) >= per_page:
                 break
 
@@ -44,9 +43,9 @@ class IBGEService:
         self, page: int, per_page: int, search: str | None = None
     ) -> list[Distrito]:
         search = search.lower() if search else None  # not case sensitive
-
-        return self._request_with_filter_and_pagination(
-            f"{self.localidades_url}/distritos",
+        raw_data = self.ibge_client.list_distritos()
+        return self._filter_and_pagination(
+            raw_data,
             page,
             per_page,
             lambda d: not search or d["nome"].lower().startswith(search),
@@ -57,9 +56,10 @@ class IBGEService:
         self, page: int, per_page: int, search: str | None = None
     ) -> list[Municipio]:
         search = search.lower() if search else None  # not case sensitive
+        raw_data = self.ibge_client.list_municipios()
 
-        return self._request_with_filter_and_pagination(
-            f"{self.localidades_url}/municipios",
+        return self._filter_and_pagination(
+            raw_data,
             page,
             per_page,
             lambda d: not search or d["nome"].lower().startswith(search),

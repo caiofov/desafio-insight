9518ec5788e574650db3ccdbfe075d37d4ade256
caiofov <cfoviana@gmail.com>
2024-06-08 18:37:49 -0300

    docs: nomes routes

--- src/app/routes/nomes.py
+++ src/app/routes/nomes.py
@@ -8,9 +8,14 @@ router = APIRouter()
 
 @router.get("")
 def get_names(
-    names: list[str] = Query(None),
+    names: list[str] = Query(),
     service: IBGEService = Depends(get_ibge_service),
 ) -> list[Nome]:
+    """Dados dos nomes listados no IBGE
+
+    - `names` (list[str]): Nomes para os quais os dados devem ser coletados
+
+    """
     return service.get_names(names)
 
 
@@ -20,4 +25,10 @@ def get_name_by_uf(
     uf_name: bool = False,
     service: IBGEService = Depends(get_ibge_service),
 ) -> list[NomeLocalidade] | list[NomeLocalidadeWithDetails]:
+    """Dados de um único nome, agrupados pela UF. Caso `uf_name` seja verdadeiro, retorna detalhes de cada UF
+
+    - `name` (str): Nome para o qual os dados devem ser coletados
+    - `uf_name` (bool, optional): Caso verdadeiro, retorna os dados de cada UF listada . Padrão é false
+
+    """
     return service.get_name_by_uf(name, uf_name)

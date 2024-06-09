8e67fe25333683a0bed9ed6bf118a49af35a2429
caiofov <cfoviana@gmail.com>
2024-06-08 22:15:14 -0300

    refactor: change get name by uf param

--- src/app/routes/nomes.py
+++ src/app/routes/nomes.py
@@ -22,13 +22,13 @@ def get_names(
 @router.get("/by_uf")
 def get_name_by_uf(
     name: str,
-    uf_name: bool = False,
+    uf_details: bool = False,
     service: IBGEService = Depends(get_ibge_service),
 ) -> list[NomeLocalidade] | list[NomeLocalidadeWithDetails]:
-    """Dados de um único nome, agrupados pela UF. Caso `uf_name` seja verdadeiro, retorna detalhes de cada UF
+    """Dados de um único nome, agrupados pela UF. Caso `uf_details` seja verdadeiro, retorna detalhes de cada UF
 
     - `name` (str): Nome para o qual os dados devem ser coletados
-    - `uf_name` (bool, optional): Caso verdadeiro, retorna os dados de cada UF listada . Padrão é false
+    - `uf_details` (bool, optional): Caso verdadeiro, retorna os dados de cada UF listada . Padrão é false
 
     """
-    return service.get_name_by_uf(name, uf_name)
+    return service.get_name_by_uf(name, uf_details)
